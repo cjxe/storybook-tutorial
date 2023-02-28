9d0590bdd9bf1ec39b133001897e91cc6eec3068
mbaranturkmen <kilexey@gmail.com>
2023-02-28 14:23:42 +0000

    finish lesson 4.4 - using `args`

--- my-app/src/components/Button/Button.stories.js
+++ my-app/src/components/Button/Button.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from './Button';
 
 // adds an entry in the side nav
+// A1: We can also the master component with `args` by adding the `args` object
 export default {
   title: 'Components/Button',
   component: Button
@@ -12,3 +13,21 @@ export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+
+// Q1: why write stories with `args`?
+const Template = args => <Button {...args} />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+  variant: 'primary',
+  children: 'Primary args'
+}
+
+// A1: extend props from other stories
+// with this, we can do more with less repetition
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  children: 'Long primary args'
+}
